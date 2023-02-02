ba6a019a276c079b5fc6aaee4c2d74d4de4361ff
harcourtian <suryanshhbtu@gmail.com>
2023-02-03 02:16:30 +0530

    Divided Main screen into 3 parts News&Event, Carouse and Activity

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 
 import Alert from './Components/layout/Alert';
+import CardView from './Components/layout/CardView';
+import Carouse from './Components/layout/Carouse';
 import MainNavigation from './Components/layout/MainNavigation';
 
 function App() {
   return (
     <div>
-      <MainNavigation/>
-      <Alert message="This Site is Under Construction Phase."/>
-    </div>
+      <MainNavigation />
+      <Alert message="This Site is Under Construction Phase." />
+      <div className="container.fluid pl-5 pr-5 pt-4" >
+        <div className="row">
+          <div className="col">
+            <CardView />
+          </div>
+          <div className="col-6">
+            <Carouse/>
+          </div>
+          <div className="col">
+            <CardView />
+          </div>
+        </div>
+      </div>
+    </div >
   );
 }
 
--- src/Components/layout/MainNavigation.js
+++ src/Components/layout/MainNavigation.js
@@ -4,7 +4,7 @@ const MainNavigation = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <a className="navbar-brand" href="/">MD ACADEMY</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
 
